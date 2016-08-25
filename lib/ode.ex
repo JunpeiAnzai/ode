0982a8b52c6505782cb2c1571776d6e973dee1d9
Jumpei Anzai <junpei.anzai@gmail.com>
2016-08-25 16:45:29 +0900

    add authentication process before redeem token

--- lib/ode.ex
+++ lib/ode.ex
@@ -8,6 +8,8 @@ defmodule Ode do
 
     config_file_path
     |> read_config
+
+    authentication
   end
 
   def parse_args(args) do
@@ -35,5 +37,43 @@ defmodule Ode do
       {:ok, body} -> IO.puts body
       {:error, reason} -> IO.puts reason
     end
+  end
+
+  def authentication() do
+    auth_url = "http://login.live.com/oauth20_authorize.srf"
+    redirect_url = "https://login.live.com/oauth20_desktop.srf"
+    token_url = "https://login.live.com/oauth20_token.srf"
+    client_id = "7ee18b85-8a43-4fe0-9a44-1b965038d3d4"
+    auth_url_full = auth_url
+    <> "?client_id="
+    <> client_id
+    <> "&scope=onedrive.readwrite%20offline_access&response_type=code&redirect_uri="
+    <> redirect_url
+
+    IO.puts "Autorize this app visiging:\n"
+    IO.puts auth_url_full
+    response = IO.gets "enter the response uri:"
+
+    String.trim(response)
+    |> get_code
+  end
+
+  def get_code(response_uri) do
+    c = Regex.run(~r/(?:code=)(([\w\d]+-){4}[\w\d]+)/,
+      response_uri)
+      |> tl
+      |> hd
+      |> validate_code
+  end
+
+  def validate_code(code) do
+    case String.length(code) do
+      0 -> IO.puts "empty"
+      _ -> redeem_token(code)
+    end
+  end
+
+  def redeem_token(code) do
+    IO.puts code
   end
 end
