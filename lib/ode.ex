c19bec98dbbc91c3db5b68a12752b7cecffa40ce
Jumpei Anzai <junpei.anzai@gmail.com>
2016-08-25 13:57:41 +0900

    add options parser

--- lib/ode.ex
+++ lib/ode.ex
@@ -1,2 +1,27 @@
 defmodule Ode do
+  def main(args \\ []) do
+    args
+    |> parse_args
+    |> process
+  end
+
+  def parse_args(args) do
+    options =
+      args
+      |> OptionParser.parse()
+    elem(options, 0)
+  end
+
+  def process([]) do
+    IO.puts "No arguments"
+  end
+
+  def process(options) do
+    case hd options do
+      {:debug, true} -> IO.puts "debug"
+      {:monitor, true} -> IO.puts "monitor"
+      _ -> IO.puts "another"
+    end
+    process (tl options)
+  end
 end
