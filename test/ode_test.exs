1732559c74f784115fe7acf8947977af0ac31bc4
Jumpei Anzai <junpei.anzai@gmail.com>
2016-08-31 16:07:44 +0900

    Add insert test

--- test/ode_test.exs
+++ test/ode_test.exs
@@ -2,7 +2,30 @@ defmodule OdeTest do
   use ExUnit.Case
   doctest Ode
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Ode.Repo
+  alias Ode.Item
+
+  import Ecto.Query
+
+  setup_all do
+    {:ok, pid} = Ode.start(nil, nil)
+    {:ok, [pid: pid]}
+  end
+
+  test "that should insert item normally" do
+    # assert we can insert and query a user
+    name = :rand.uniform |> to_string
+    {:ok, some_item} = %Item{name: name,
+                          type: "item_type",
+                          etag: "item_etag",
+                          ctag: "item_ctag",
+                          mtime: "item_mtime",
+                          crc32: "item_crc32"}
+                          |> Repo.insert
+    [name] =
+      Item
+      |> select([item], item.name)
+      |> where([item], item.name == ^some_item.name)
+      |> Repo.all
   end
 end
