282126543117fbee0c02f781e7b9fd7d04b8c074
Jumpei Anzai <junpei.anzai@gmail.com>
2016-08-31 17:16:37 +0900

    add dao

---
+++ lib/item_db.ex
@@ -0,0 +1,13 @@
+defmodule ItemDB do
+  @file_type {:file, :dir}
+  def insert
+  def update
+  def upsert
+  def selectChildren
+  def selectById
+  def selectByPath
+  def deleteById
+  def hasParent
+  def buildItem
+  def computePath
+end
